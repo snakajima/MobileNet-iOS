0f189b38a77e5f533e49bfff16cd1b7f18d0a4cd
SATOSHI NAKAJIMA <satoshi.nakajima@gmail.com>
2017-07-25 14:59:57 -0700

    almost

--- MobileNet/MobileNet/ViewController.swift
+++ MobileNet/MobileNet/ViewController.swift
@@ -8,29 +8,50 @@
 
 import UIKit
 import AVFoundation
+import Vision
 
 class ViewController: UIViewController {
+    let model = MobileNet()
+    lazy var visionModel = try? VNCoreMLModel(for: self.model.model)
+    var request:VNCoreMLRequest?
     lazy var session:VSCaptureSession = VSCaptureSession(device: MTLCreateSystemDefaultDevice()!, pixelFormat: MTLPixelFormat.a8Unorm, delegate: self)
+    var sampleBuffer:CMSampleBuffer?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        if let visionModel = self.visionModel {
+            request = VNCoreMLRequest(model: visionModel) { request, error in
+                if let observations = request.results as? [VNClassificationObservation] {
+                    // The observations appear to be sorted by confidence already, so we
+                    // take the top 5 and map them to an array of (String, Double) tuples.
+                    let top5 = observations.prefix(through: 4)
+                        .map { ($0.identifier, Double($0.confidence)) }
+                    print(top5)
+                }
+                self.sampleBuffer = nil
+            }
+            request!.imageCropAndScaleOption = .centerCrop
+        }
         session.start()
         
         let previewLayer = AVCaptureVideoPreviewLayer(session: session.session!)
         previewLayer.frame = self.view.bounds
         self.view.layer.addSublayer(previewLayer)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
 extension ViewController : VSCaptureSessionDelegate {
     func didCaptureOutput(session:VSCaptureSession, texture textureIn:MTLTexture, sampleBuffer:CMSampleBuffer, presentationTime:CMTime) {
+        if self.sampleBuffer != nil {
+            print("skip")
+            return
+        }
+        if let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer),
+           let request = self.request {
+            self.sampleBuffer = sampleBuffer
+            let handler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, orientation: .up, options: [:])
+            try? handler.perform([request])
+        }
     }
 }
